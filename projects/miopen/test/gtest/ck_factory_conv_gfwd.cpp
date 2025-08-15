a668d46dfcc39e7a02d2d67a83a047912a5a9e0a
John Shumway <jshumway@amd.com>
2025-08-15 03:02:36 +0000

    Implement CK factory tests with parmeterized types.
    Refactor CK factory tests to validate expected instance counts for grouped convolution forward operations. Hard-coded to capture the current library results.

--- projects/miopen/test/gtest/ck_factory_conv_gfwd.cpp
+++ projects/miopen/test/gtest/ck_factory_conv_gfwd.cpp
@@ -2,8 +2,8 @@
 //
 // ## Purpose:
 // Checks that the CK factory for grouped convolution forward operations (with different
-// epilogue types: PassThrough, Scale, Bilinear) does NOT create any instances for the tested
-// data types (float16_t, float32_t).
+// epilogue types: PassThrough, Scale, Bilinear) creates the expected numbers of instances for the
+// tested data types (fp16, fp32, bf16, s8).
 //
 // ## How:
 // Each test calls GetInstances() on a factory type for a specific combination of data type and
@@ -14,9 +14,12 @@
 // available kernels we should update this test.
 //
 // ## Tested Types:
-// - Data types: c:float16_t, ck::float32_t
+// - Data types:
+//   - fp16: ck::half_t
+//   - fp32: float
+//   - bf16: ck::bhalf_t
+//   - s8: int8_t
 // - Epilogues: PassThrough, Scale, Bilinear
-// *note: these are the only types that compiled and linked correctly.*
 #include "miopen/config.h"
 #if MIOPEN_BACKEND_HIP && MIOPEN_USE_COMPOSABLEKERNEL
 #include <gtest/gtest.h>
@@ -76,41 +79,120 @@ template <typename DataType, typename Epilogue>
 using DeviceOpGFwdPtrs = ck::tensor_operation::device::instance::DeviceOperationInstanceFactory<
     typename DeviceOpGFwdHelper<DataType, Epilogue>::type>;
 
-TEST(CkFactoryTest, CreatesGrpConvFwdPassThroughF16Instances)
+template <typename T>
+class CkFactoryTest : public testing::Test
 {
-    auto conv_ptrs = DeviceOpGFwdPtrs<ck::float16_t, PassThrough>::GetInstances();
-    ASSERT_EQ(conv_ptrs.size(), 0);
+};
+
+struct Fp16PassThrough
+{
+    using DataType             = ck::half_t;
+    using Epilogue             = PassThrough;
+    static constexpr int COUNT = 228;
+};
+
+struct Fp32PassThrough
+{
+    using DataType             = float;
+    using Epilogue             = PassThrough;
+    static constexpr int COUNT = 173;
+};
+
+struct Bf16PassThrough
+{
+    using DataType             = ck::bhalf_t;
+    using Epilogue             = PassThrough;
+    static constexpr int COUNT = 237;
+};
+
+struct S8PassThrough
+{
+    using DataType             = int8_t;
+    using Epilogue             = PassThrough;
+    static constexpr int COUNT = 48;
+};
+
+struct Fp16Scale
+{
+    using DataType             = ck::half_t;
+    using Epilogue             = Scale;
+    static constexpr int COUNT = 48;
+};
+
+struct Fp32Scale
+{
+    using DataType             = float;
+    using Epilogue             = Scale;
+    static constexpr int COUNT = 48;
+};
+
+struct Bf16Scale
+{
+    using DataType             = ck::bhalf_t;
+    using Epilogue             = Scale;
+    static constexpr int COUNT = 48;
+};
+
+struct S8Scale
+{
+    using DataType             = int8_t;
+    using Epilogue             = Scale;
+    static constexpr int COUNT = 48;
+};
+
+struct Fp16Bilinear
+{
+    using DataType             = ck::half_t;
+    using Epilogue             = Bilinear;
+    static constexpr int COUNT = 48;
+};
+
+struct Fp32Bilinear
+{
+    using DataType             = float;
+    using Epilogue             = Bilinear;
+    static constexpr int COUNT = 48;
+};
+
+struct Bf16Bilinear
+{
+    using DataType             = ck::bhalf_t;
+    using Epilogue             = Bilinear;
+    static constexpr int COUNT = 48;
+};
+
+struct S8Bilinear
+{
+    using DataType             = int8_t;
+    using Epilogue             = Bilinear;
+    static constexpr int COUNT = 48;
+};
+
+using MyTypes = ::testing::Types<Fp16PassThrough,
+                                 Fp32PassThrough,
+                                 Bf16PassThrough,
+                                 S8PassThrough,
+                                 Fp16Scale,
+                                 Fp32Scale,
+                                 Bf16Scale,
+                                 S8Scale,
+                                 Fp16Bilinear,
+                                 Fp32Bilinear,
+                                 Bf16Bilinear,
+                                 S8Bilinear>;
+
+TYPED_TEST_SUITE_P(CkFactoryTest);
+
+TYPED_TEST_P(CkFactoryTest, CreatesGrpConvFwdInstances)
+{
+    using DataType = typename TypeParam::DataType;
+    using Epilogue = typename TypeParam::Epilogue;
+    auto conv_ptrs = DeviceOpGFwdPtrs<DataType, Epilogue>::GetInstances();
+    ASSERT_EQ(conv_ptrs.size(), TypeParam::COUNT);
 }
 
-TEST(CkFactoryTest, CreatesGrpConvFwdPassThroughF32Instances)
-{
-    auto conv_ptrs = DeviceOpGFwdPtrs<ck::float32_t, PassThrough>::GetInstances();
-    ASSERT_EQ(conv_ptrs.size(), 0);
-}
-
-TEST(CkFactoryTest, CreatesGrpConvFwdScaleF16Instances)
-{
-    auto conv_ptrs = DeviceOpGFwdPtrs<ck::float16_t, Scale>::GetInstances();
-    ASSERT_EQ(conv_ptrs.size(), 0);
-}
-
-TEST(CkFactoryTest, CreatesGrpConvFwdScaleF32Instances)
-{
-    auto conv_ptrs = DeviceOpGFwdPtrs<ck::float32_t, Scale>::GetInstances();
-    ASSERT_EQ(conv_ptrs.size(), 0);
-}
-
-TEST(CkFactoryTest, CreatesGrpConvFwdBilinearF16Instances)
-{
-    auto conv_ptrs = DeviceOpGFwdPtrs<ck::float16_t, Bilinear>::GetInstances();
-    ASSERT_EQ(conv_ptrs.size(), 0);
-}
-
-TEST(CkFactoryTest, CreatesGrpConvFwdBilinearF32Instances)
-{
-    auto conv_ptrs = DeviceOpGFwdPtrs<ck::float32_t, Bilinear>::GetInstances();
-    ASSERT_EQ(conv_ptrs.size(), 0);
-}
+REGISTER_TYPED_TEST_SUITE_P(CkFactoryTest, CreatesGrpConvFwdInstances);
+INSTANTIATE_TYPED_TEST_SUITE_P(Full, CkFactoryTest, MyTypes);
 
 } // namespace
 } // namespace miopen::unit_test
